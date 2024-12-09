d55ac0a4458e03a9eae9ff3c89a7cfabdc98b266
ZeroHopeV <none lmao>
2024-12-09 14:38:18 +0100

    update classes with documentation

--- player.py
+++ player.py
@@ -1,5 +1,13 @@
 class Player:
 
+    """
+    Class for Player
+
+    Parameters:
+    name (str): name
+    hp = 10 (int): health
+    """
+
     # Lagre navn og HP
     def __init__(self, name:str):
         self.name = name
